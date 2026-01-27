00f8dd522d751435c5aae3c6d72a59cd842eaf35
Kiren Paul <kirenpaul@gmail.com>
2026-01-27 08:17:45 +0700

    Add FinChronicleMainView and CodeBreaker files; refactor MatchMarkers for clarity

---
+++ FinChronicle/FinChronicleViews/FinChronicleMainView.swift
@@ -0,0 +1,18 @@
+//
+//  FinChronicleMainView.swift
+//  FinChronicle
+//
+//  Created by Paul, Kiren (Allianz Technology) on 26/1/26.
+//
+
+import SwiftUI
+
+struct FinChronicleMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinChronicleMainView()
+}
