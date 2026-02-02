e3370652d7c1ea7327a4b98446bf70a84cd174fa
Kiren Paul <kirenpaul@gmail.com>
2026-02-02 13:48:17 +0700

    Add PegChooser view with initial implementation

---
+++ FinChronicle/UI/PegChooser.swift
@@ -0,0 +1,18 @@
+//
+//  PegChooser.swift
+//  FinChronicle
+//
+//  Created by Paul, Kiren on 2/2/26.
+//
+
+import SwiftUI
+
+struct PegChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PegChooser()
+}
